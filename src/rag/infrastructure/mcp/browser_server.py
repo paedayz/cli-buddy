fede1229daccba09dfdf638948c87a967041494c
paedayz <pae16phasit@gmail.com>
2025-04-24 23:34:40 +0700

    chore: remove unuse comment in browser server

--- src/rag/infrastructure/mcp/browser_server.py
+++ src/rag/infrastructure/mcp/browser_server.py
@@ -2,7 +2,6 @@ from mcp.server.fastmcp import FastMCP
 import webbrowser
 import urllib.parse
 
-
 mcp = FastMCP("Browser")
     
 @mcp.tool()
@@ -16,10 +15,7 @@ def open_youtube_search(keyword):
     query = urllib.parse.quote(keyword)
     url = f"https://www.youtube.com/results?search_query={query}"
     
-    # Specify path to Chrome if needed, otherwise use default browser
-    chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"  # Windows
-    # chrome_path = "/usr/bin/google-chrome %s"  # Linux
-    # chrome_path = "open -a /Applications/Google\\ Chrome.app %s"  # macOS
+    chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 
     webbrowser.get(chrome_path).open(url)
     
